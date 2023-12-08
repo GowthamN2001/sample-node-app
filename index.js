b1463c4bee3e2bc8345df0983476d45c7aeac9c0
Gowtham <102454841+GowthamN2001@users.noreply.github.com>
2023-12-08 10:26:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello this is nodejs</h1>');
+    res.send('<h1>Welcome this is nodejs</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
